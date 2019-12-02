cdf312b0cb1e96827e4f74ad6aee184e9a382535
Johan Mattsson <johan.mattsson@hotmail.se>
2019-12-02 10:59:00 +0100

    Implement empty methods and constructor

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,4 +1,23 @@
 package com.company;
 
 public class Human extends Player {
+
+    public Human(String name, BoardGame map) {
+        super(name, map);
+    }
+
+    @Override
+    public void placeShips() {
+
+    }
+
+    @Override
+    public void ShowCoordinates() {
+
+    }
+
+    @Override
+    public void shoot() {
+
+    }
 }
