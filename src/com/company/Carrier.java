e5fd156b87cf88cbbef60840d3ed8fbc37bcebbf
Anna Söllscher <annamaria.sollscher@gmail.com>
2019-12-02 11:57:21 +0100

    Gjorde klassen carrier samt la till get i player

--- src/com/company/Carrier.java
+++ src/com/company/Carrier.java
@@ -1,4 +1,18 @@
 package com.company;
 
-public class Carrier {
+import java.util.ArrayList;
+
+public class Carrier extends Ship {
+    // extends Ship (Superclass)
+
+    public Carrier(ArrayList<Position> positions) {
+        //constructor
+
+        super(positions);
+    }
+
+    @Override
+    public void wasHit() {
+
+    }
 }
